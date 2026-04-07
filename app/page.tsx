e43f46eb71853ffd13600ca7c7e784b6214644f9
Sajid Bhati <sajidbhati15511@gmail.com>
2026-04-07 22:50:31 +0530

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
 export default function Home() {
-  // const a = ;
+  const a = 10;
   return (
     <div>
       <Navbar />
